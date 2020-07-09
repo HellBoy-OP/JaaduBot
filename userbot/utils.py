38817ac5f16c3065f34b87a5b27aabe0f63fec4a
Ranger OP <59554677+Amberyt@users.noreply.github.com>
2020-07-09 20:21:45 +0530

    Update utils.py

--- userbot/utils.py
+++ userbot/utils.py
@@ -2,6 +2,7 @@ from userbot import bot
 from telethon import events
 from var import Var
 from pathlib import Path
+from userbot import userbot.modules
 from userbot.uniborgConfig import Config
 from userbot import LOAD_PLUG
 from userbot import CMD_LIST
