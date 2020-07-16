5ba8a70944f9a34d74369b69bb2a04eb064ecb19
Ranger OP <59554677+Amberyt@users.noreply.github.com>
2020-07-16 10:02:43 +0530

    Update memes.py

--- userbot/plugins/memes.py
+++ userbot/plugins/memes.py
@@ -758,7 +758,7 @@ async def slap(replied_user, event):
     hit = random.choice(HIT)
     throw = random.choice(THROW)
 
-    caption = "..." + temp.format(victim=slapped, item=item, hits=hit, throws=throw)
+    caption = "{DEFAULTUSER}" + temp.format(victim=slapped, item=item, hits=hit, throws=throw)
 
     return caption
 
