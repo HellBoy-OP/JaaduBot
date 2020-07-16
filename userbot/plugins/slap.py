7cfad40cdc15de883aaf390c7fa0f1cca5bba242
Ranger OP <59554677+Amberyt@users.noreply.github.com>
2020-07-16 09:45:57 +0530

    Update slap.py

--- userbot/plugins/slap.py
+++ userbot/plugins/slap.py
@@ -13,17 +13,17 @@ from telethon.tl.types import MessageEntityMentionName
 from userbot import ALIVE_NAME
 
 SLAP_TEMPLATES = [
-    "{user1} {hits} {user2} with a {item}.",
-    "{user1} {hits} {user2} in the face with a {item}.",
-    "{user1} {hits} {user2} around a bit with a {item}.",
-    "{user1} {throws} a {item} at {user2}.",
-    "{user1} grabs a {item} and {throws} it at {user2}'s face.",
-    "{user1} launches a {item} in {user2}'s general direction.",
-    "{user1} starts slapping {user2} silly with a {item}.",
-    "{user1} pins {user2} down and repeatedly {hits} them with a {item}.",
-    "{user1} grabs up a {item} and {hits} {user2} with it.",
-    "{user1} ties {user2} to a chair and {throws} a {item} at them.",
-    "{user1} gave a friendly push to help {user2} learn to swim in lava."
+    "{DEFAULTUSER} {hits} {user2} with a {item}.",
+    "{DEFAULTUSER} {hits} {user2} in the face with a {item}.",
+    "{DEFAULTUSER} {hits} {user2} around a bit with a {item}.",
+    "{DEFAULTUSER} {throws} a {item} at {user2}.",
+    "{DEFAULTUSER} grabs a {item} and {throws} it at {user2}'s face.",
+    "{DEFAULTUSER} launches a {item} in {user2}'s general direction.",
+    "{DEFAULTUSER} starts slapping {user2} silly with a {item}.",
+    "{DEFAULTUSER} pins {user2} down and repeatedly {hits} them with a {item}.",
+    "{DEFAULTUSER} grabs up a {item} and {hits} {user2} with it.",
+    "{DEFAULTUSER} ties {user2} to a chair and {throws} a {item} at them.",
+    "{DEFAULTUSER} gave a friendly push to help {user2} learn to swim in lava."
 ]
 
 ITEMS = [
