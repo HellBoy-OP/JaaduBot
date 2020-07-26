d65491ea329dafa15f566d5ccf0e3f60f50b5619
Ranger OP <59554677+Amberyt@users.noreply.github.com>
2020-07-26 21:36:21 +0530

    Update remove_bg.py

--- userbot/plugins/remove_bg.py
+++ userbot/plugins/remove_bg.py
@@ -72,9 +72,9 @@ async def _(event):
             )
         end = datetime.now()
         ms = (end - start).seconds
-        await event.edit("Removed dat annoying Backgroup in {} seconds".format(ms))
+        await event.edit("Removed dat annoying Background in {} seconds"
     else:
-        await event.edit("ReMove.BG API returned Errors. Please report to @jaadu_bot_support\n`{}".format(output_file_name.content.decode("UTF-8")))
+        await event.edit("ReMove.BG API returned Errors. Please report to @jaadu_bot_support\n`{}"
 
 
 # this method will call the API, and return in the appropriate format
