899031d9599c86760cc446b8fa5db39d7bca8c7d
Ranger OP <59554677+Amberyt@users.noreply.github.com>
2020-08-02 18:33:24 +0530

    Update mafiacrazykang.py

--- userbot/plugins/mafiacrazykang.py
+++ userbot/plugins/mafiacrazykang.py
@@ -105,7 +105,7 @@ async def kang(args):
                 # pack
                 emoji = splat[1]
 
-        packname = f"{user.id}_by_Jaadu_{pack}"
+        packname = f"ranger{userid}_by_Jaadu_{pack}"
         packnick = f"@{user.username}'s kang pack Vol.{pack}"
         cmd = '/newpack'
         file = io.BytesIO()
@@ -133,7 +133,7 @@ async def kang(args):
                 x = await conv.get_response()
                 while x.text == PACK_FULL:
                     pack += 1
-                    packname = f"{user.id}_by_Jaadu_{pack}"
+                    packname = f"ranger{user.id}_by_Jaadu_{pack}"
                     packnick = f"@{user.username}'s kang pack Vol.{pack}"
                     await args.edit(f"`{kang_meme}\
                     \nMoving on to Vol.{str(pack)}..`")
