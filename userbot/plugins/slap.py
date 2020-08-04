55c72fe5f5ac583cabffe48b0e4f4ae5ac096e2c
Ranger OP <59554677+Amberyt@users.noreply.github.com>
2020-08-04 10:48:56 +0530

    Update slap.py

--- userbot/plugins/slap.py
+++ userbot/plugins/slap.py
@@ -11,6 +11,7 @@ import random
 from telethon.tl.functions.users import GetFullUserRequest
 from telethon.tl.types import MessageEntityMentionName
 from userbot import ALIVE_NAME
+from userbot import SLAP_USERNAME
 
 SLAP_TEMPLATES = [
     "{user1} {hits} {user2} with a {item}.",
@@ -73,7 +74,7 @@ HIT = [
     "bashes",
 ]
 
-user1 = str(ALIVE_NAME) if ALIVE_NAME else "JaaduBot"
+user1 = str(SLAP_USERNAME) if ALIVE_NAME else "JaaduBot"
 
 @borg.on(admin_cmd(pattern="slap ?(.*)", allow_sudo=True))
 async def who(event):
