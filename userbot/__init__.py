0b2ae9295e816906b773b97f1d032215cd32de44
Ranger OP <59554677+Amberyt@users.noreply.github.com>
2020-08-05 08:07:14 +0530

    Update __init__.py

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -1,128 +1,134 @@
-# Copyright (C) 2019 The Raphielscape Company LLC.
-#
-# Licensed under the Raphielscape Public License, Version 1.d (the "License");
-# you may not use this file except in compliance with the License.
-#
-""" Userbot initialization. """
-
 import os
-from distutils.util import strtobool as sb
-from logging import DEBUG, INFO, basicConfig, getLogger
-from sys import version_info
+import sys
+from telethon.sessions import StringSession
+from telethon import TelegramClient
 
-from dotenv import load_dotenv
-from pyDownload import Downloader
-from pylast import LastFMNetwork, md5
+from var import Var
+
+os.system("pip install --upgrade pip")
+if Var.STRING_SESSION:
+    session_name = str(Var.STRING_SESSION)
+    bot = TelegramClient(StringSession(session_name), Var.APP_ID, Var.API_HASH)
+else:
+    session_name = "startup"
+    bot = TelegramClient(session_name, Var.APP_ID, Var.API_HASH)
+
+
+CMD_LIST = {}
+# for later purposes
+CMD_HELP = {}
+INT_PLUG = ""
+LOAD_PLUG = {}
+
+# PaperPlaneExtended Support Vars
+ENV = os.environ.get("ENV", False)
+""" PPE initialization. """
+
+from logging import basicConfig, getLogger, INFO, DEBUG
+from distutils.util import strtobool as sb
+import asyncio
+
+import pylast
+from pySmartDL import SmartDL
 from pymongo import MongoClient
 from redis import StrictRedis
 from requests import get
-from telethon import TelegramClient
-from telethon.sessions import StringSession
-
-load_dotenv("config.env")
-
 # Bot Logs setup:
-CONSOLE_LOGGER_VERBOSE = sb(os.environ.get("CONSOLE_LOGGER_VERBOSE", "False"))
+if bool(ENV):
+    CONSOLE_LOGGER_VERBOSE = sb(os.environ.get("CONSOLE_LOGGER_VERBOSE", "False"))
 
-if CONSOLE_LOGGER_VERBOSE:
-    basicConfig(
-        format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
-        level=DEBUG,
-    )
-else:
-    basicConfig(format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
-                level=INFO)
-LOGS = getLogger(__name__)
+    if CONSOLE_LOGGER_VERBOSE:
+        basicConfig(
+            format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+            level=DEBUG,
+        )
+    else:
+        basicConfig(format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+                    level=INFO)
+    LOGS = getLogger(__name__)
 
-if version_info[0] < 3 or version_info[1] < 6:
-    LOGS.error("You MUST have a python version of at least 3.6."
-               " Multiple features depend on this. Bot quitting.")
-    quit(1)
+    # Check if the config was edited by using the already used variable.
+    # Basically, its the 'virginity check' for the config file ;)
+    CONFIG_CHECK = os.environ.get(
+        "___________PLOX_______REMOVE_____THIS_____LINE__________", None)
 
-# Check if the config was edited by using the already used variable
-CONFIG_CHECK = os.environ.get(
-    "___________PLOX_______REMOVE_____THIS_____LINE__________", None)
-
-if CONFIG_CHECK:
-    LOGS.error("Please remove the line mentioned in the first \
-         hashtag from the config.env file")
-    quit(1)
-
-API_KEY = os.environ.get("API_KEY", None)
-
-API_HASH = os.environ.get("API_HASH", None)
-
-STRING_SESSION = os.environ.get("STRING_SESSION", None)
-
-BOTLOG = sb(os.environ.get("BOTLOG", "False"))
-
-BOTLOG_CHATID = int(os.environ.get("BOTLOG_CHATID")) if BOTLOG else 0
-
-PM_AUTO_BAN = sb(os.environ.get("PM_AUTO_BAN", "False"))
-
-MONGO_DB_URI = os.environ.get("MONGO_DB_URI", None)
-
-SCREENSHOT_LAYER_ACCESS_KEY = os.environ.get("SCREENSHOT_LAYER_ACCESS_KEY",
-                                             None)
-
-OPEN_WEATHER_MAP_APPID = os.environ.get("OPEN_WEATHER_MAP_APPID", None)
-print(OPEN_WEATHER_MAP_APPID)
-
-WELCOME_MUTE = sb(os.environ.get("WELCOME_MUTE", "False"))
-
-SPOTIFY_USERNAME = os.environ.get("SPOTIFY_USERNAME", None)
-SPOTIFY_PASS = os.environ.get("SPOTIFY_PASS", None)
-BIO_PREFIX = os.environ.get("BIO_PREFIX", None)
-DEFAULT_BIO = os.environ.get("DEFAULT_BIO", None)
-
-LASTFM_API = os.environ.get("LASTFM_API", None)
-LASTFM_SECRET = os.environ.get("LASTFM_SECRET", None)
-LASTFM_USERNAME = os.environ.get("LASTFM_USERNAME", None)
-LASTFM_PASSWORD_PLAIN = os.environ.get("LASTFM_PASSWORD", None)
-LASTFM_PASS = md5(LASTFM_PASSWORD_PLAIN)
-if not LASTFM_USERNAME == "None":
-    lastfm = LastFMNetwork(api_key=LASTFM_API,
-                           api_secret=LASTFM_SECRET,
-                           username=LASTFM_USERNAME,
-                           password_hash=LASTFM_PASS)
-else:
-    lastfm = None
-
-GDRIVE_FOLDER = os.environ.get("GDRIVE_FOLDER", None)
-
-HEROKU_APIKEY = os.environ.get("HEROKU_APIKEY", None)
-HEROKU_APPNAME = os.environ.get("HEROKU_APPNAME", None)
-
-WOLFRAM_ID = os.environ.get("WOLFRAM_ID", None)
-
-# pylint: disable=invalid-name
-if STRING_SESSION:
-    bot = TelegramClient(StringSession(STRING_SESSION), API_KEY, API_HASH)
-else:
-    bot = TelegramClient("userbot", API_KEY, API_HASH)
-
-
-async def check_botlog_chatid():
-    if not BOTLOG:
-        return
-
-    entity = await bot.get_entity(BOTLOG_CHATID)
-    if entity.default_banned_rights.send_messages:
-        LOGS.error(
-            "Your account doesn't have rights to send messages to BOTLOG_CHATID "
-            "group. Check if you typed the Chat ID correctly.")
+    if CONFIG_CHECK:
+        LOGS.info(
+            "Please remove the line mentioned in the first hashtag from the config.env file"
+        )
         quit(1)
 
-
-with bot:
+    # Logging channel/group configuration.
+    BOTLOG_CHATID = os.environ.get("BOTLOG_CHATID", None)
     try:
-        bot.loop.run_until_complete(check_botlog_chatid())
+        BOTLOG_CHATID = int(BOTLOG_CHATID)
     except:
-        LOGS.error("BOTLOG_CHATID environment variable isn't a "
-                   "valid entity. Check your config.env file.")
-        quit(1)
+        pass
 
-# Init Mongo
+    # Userbot logging feature switch.
+    BOTLOG = sb(os.environ.get("BOTLOG", "False"))
+
+    # Bleep Blop, this is a bot ;)
+    PM_AUTO_BAN = sb(os.environ.get("PM_AUTO_BAN", "False"))
+
+    # Console verbose logging
+    CONSOLE_LOGGER_VERBOSE = sb(os.environ.get("CONSOLE_LOGGER_VERBOSE", "False"))
+
+    # SQL Database URI
+    DB_URI = os.environ.get("DATABASE_URL", None)
+
+    # OCR API key
+    OCR_SPACE_API_KEY = os.environ.get("OCR_SPACE_API_KEY", None)
+
+    # remove.bg API key
+    REM_BG_API_KEY = os.environ.get("REM_BG_API_KEY", None)
+
+    # Chrome Driver and Headless Google Chrome Binaries
+    CHROME_DRIVER = os.environ.get("CHROME_DRIVER", None)
+    GOOGLE_CHROME_BIN = os.environ.get("GOOGLE_CHROME_BIN", None)
+
+    # OpenWeatherMap API Key
+    OPEN_WEATHER_MAP_APPID = os.environ.get("OPEN_WEATHER_MAP_APPID", None)
+
+    # Anti Spambot Config
+    ANTI_SPAMBOT = sb(os.environ.get("ANTI_SPAMBOT", "False"))
+
+    ANTI_SPAMBOT_SHOUT = sb(os.environ.get("ANTI_SPAMBOT_SHOUT", "False"))
+
+    # Youtube API key
+    YOUTUBE_API_KEY = os.environ.get("YOUTUBE_API_KEY", None)
+    #Slap Username
+    SLAP_USERNAME = os.environ.get("SLAP_USERNAME", None)
+    # Default .alive name
+    ALIVE_NAME = os.environ.get("ALIVE_NAME", None)
+    AUTONAME = os.environ.get("AUTONAME", None)
+
+    # Time & Date - Country and Time Zone
+    COUNTRY = str(os.environ.get("COUNTRY", ""))
+
+    TZ_NUMBER = int(os.environ.get("TZ_NUMBER", 1))
+
+    # Clean Welcome
+    CLEAN_WELCOME = sb(os.environ.get("CLEAN_WELCOME", "True"))
+
+    # Last.fm Module
+    BIO_PREFIX = os.environ.get("BIO_PREFIX", None)
+    DEFAULT_BIO = os.environ.get("DEFAULT_BIO", None)
+
+    LASTFM_API = os.environ.get("LASTFM_API", None)
+    LASTFM_SECRET = os.environ.get("LASTFM_SECRET", None)
+    LASTFM_USERNAME = os.environ.get("LASTFM_USERNAME", None)
+    LASTFM_PASSWORD_PLAIN = os.environ.get("LASTFM_PASSWORD", None)
+    LASTFM_PASS = pylast.md5(LASTFM_PASSWORD_PLAIN)
+    if not LASTFM_USERNAME == "None":
+        lastfm = pylast.LastFMNetwork(api_key=LASTFM_API,
+                                      api_secret=LASTFM_SECRET,
+                                      username=LASTFM_USERNAME,
+                                      password_hash=LASTFM_PASS)
+    else:
+        lastfm = None
+
+   # Init Mongo
 MONGOCLIENT = MongoClient(MONGO_DB_URI, 27017, serverSelectionTimeoutMS=1)
 MONGO = MONGOCLIENT.userbot
 
@@ -149,97 +155,39 @@ def is_redis_alive():
     except BaseException:
         return False
 
+ # Google Drive Module
+    G_DRIVE_CLIENT_ID = os.environ.get("G_DRIVE_CLIENT_ID", None)
+    G_DRIVE_CLIENT_SECRET = os.environ.get("G_DRIVE_CLIENT_SECRET", None)
+    G_DRIVE_AUTH_TOKEN_DATA = os.environ.get("G_DRIVE_AUTH_TOKEN_DATA", None)
+    GDRIVE_FOLDER_ID = os.environ.get("GDRIVE_FOLDER_ID", None)
+    TEMP_DOWNLOAD_DIRECTORY = os.environ.get("TEMP_DOWNLOAD_DIRECTORY",
+                                         "./downloads")
+else:
+    # Put your ppe vars here if you are using local hosting
+    PLACEHOLDER = None
 
-# Download binaries for gen_direct_links module, give correct perms
+# Setting Up CloudMail.ru and MEGA.nz extractor binaries,
+# and giving them correct perms to work properly.
 if not os.path.exists('bin'):
     os.mkdir('bin')
 
-url1 = 'https://raw.githubusercontent.com/yshalsager/megadown/master/megadown'
-url2 = 'https://raw.githubusercontent.com/yshalsager/cmrudl.py/master/cmrudl.py'
+binaries = {
+    "https://raw.githubusercontent.com/yshalsager/megadown/master/megadown":
+    "bin/megadown",
+    "https://raw.githubusercontent.com/yshalsager/cmrudl.py/master/cmrudl.py":
+    "bin/cmrudl"
+}
 
-dl1 = Downloader(url=url1, filename="bin/megadown")
-dl1 = Downloader(url=url1, filename="bin/cmrudl")
-
-os.chmod('bin/megadown', 0o755)
-os.chmod('bin/cmrudl', 0o755)
+for binary, path in binaries.items():
+    downloader = SmartDL(binary, path, progress_bar=False)
+    downloader.start()
+    os.chmod(path, 0o755)
 
 # Global Variables
 COUNT_MSG = 0
 USERS = {}
 COUNT_PM = {}
 LASTMSG = {}
-ENABLE_KILLME = True
 CMD_HELP = {}
-AFKREASON = "no reason"
-ZALG_LIST = [[
-    "̖",
-    " ̗",
-    " ̘",
-    " ̙",
-    " ̜",
-    " ̝",
-    " ̞",
-    " ̟",
-    " ̠",
-    " ̤",
-    " ̥",
-    " ̦",
-    " ̩",
-    " ̪",
-    " ̫",
-    " ̬",
-    " ̭",
-    " ̮",
-    " ̯",
-    " ̰",
-    " ̱",
-    " ̲",
-    " ̳",
-    " ̹",
-    " ̺",
-    " ̻",
-    " ̼",
-    " ͅ",
-    " ͇",
-    " ͈",
-    " ͉",
-    " ͍",
-    " ͎",
-    " ͓",
-    " ͔",
-    " ͕",
-    " ͖",
-    " ͙",
-    " ͚",
-    " ",
-],
-             [
-                 " ̍", " ̎", " ̄", " ̅", " ̿", " ̑", " ̆", " ̐", " ͒", " ͗",
-                 " ͑", " ̇", " ̈", " ̊", " ͂", " ̓", " ̈́", " ͊", " ͋", " ͌",
-                 " ̃", " ̂", " ̌", " ͐", " ́", " ̋", " ̏", " ̽", " ̉", " ͣ",
-                 " ͤ", " ͥ", " ͦ", " ͧ", " ͨ", " ͩ", " ͪ", " ͫ", " ͬ", " ͭ",
-                 " ͮ", " ͯ", " ̾", " ͛", " ͆", " ̚"
-             ],
-             [
-                 " ̕",
-                 " ̛",
-                 " ̀",
-                 " ́",
-                 " ͘",
-                 " ̡",
-                 " ̢",
-                 " ̧",
-                 " ̨",
-                 " ̴",
-                 " ̵",
-                 " ̶",
-                 " ͜",
-                 " ͝",
-                 " ͞",
-                 " ͟",
-                 " ͠",
-                 " ͢",
-                 " ̸",
-                 " ̷",
-                 " ͡",
-             ]]
+ISAFK = False
+AFKREASON = None
