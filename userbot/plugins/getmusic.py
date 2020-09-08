7e6790a271e56f769a2c3c581756dbf76eda3fd9
Ranger OP <59554677+Amberyt@users.noreply.github.com>
2020-09-08 23:15:53 +0530

    Update getmusic.py

--- userbot/plugins/getmusic.py
+++ userbot/plugins/getmusic.py
@@ -1,64 +1,52 @@
-""" 
-created by @mrconfused
-syntax :- .song song name 
-     
-"""
-import os
-from bs4 import BeautifulSoup
-import requests
-from telethon import events
-import subprocess
-from telethon.errors import MessageEmptyError, MessageTooLongError, MessageNotModifiedError
-import io
-import asyncio
-from userbot.utils import admin_cmd
-import glob
-import os  
+# Originally from Bothub
+# Port to UserBot by @ranger_op
+#Copyright (C) 2020 azrim.
 
-@borg.on(admin_cmd(pattern="song(?: |$)(.*)"))
-async def _(event):
-    reply_to_id = event.message.id
-    if event.reply_to_msg_id:
-        reply_to_id = event.reply_to_msg_id
-    reply = await event.get_reply_message()
-    if event.pattern_match.group(1):
-        query = event.pattern_match.group(1)
-        await event.edit("wi8..! I am finding your song....")
-    elif reply.message:
-        query = reply.message
-        await event.edit("wi8..! I am finding your song....")
-    else:
-    	await event.edit("`What I am Supposed to find `")
-    	return
-    await catmusic(str(query),"128k")
-    l = glob.glob("*.mp3")
-    if l:
-        await event.edit("yeah..! i found something wi8..🥰")
-    else:
-        await event.edit(f"Sorry..! i can't find anything with `{query}`")
-    loa = l[0]    
-    await borg.send_file(
-                event.chat_id,
-                loa,
-                force_document=True,
-                allow_cache=False,
-                caption=query,
-                reply_to=reply_to_id
-            )
-    await event.delete()
-    os.system("rm -rf *.mp3")
-    subprocess.check_output("rm -rf *.mp3",shell=True)
+from telethon import events
+import asyncio
+#from userbot.utils import admin_cmd
+from userbot.events import register 
+from userbot import bot, CMD_HELP
+from telethon.errors.rpcerrorlist import YouBlockedUserError
+import os
+try:
+ import subprocess
+except:
+ os.system("pip install instantmusic")
+ 
+
+
+os.system("rm -rf *.mp3")
+
+
+def bruh(name):
     
-async def catmusic(cat , QUALITY):
-  search = cat
-  headers = {'User-Agent': 'Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)'}
-  html = requests.get('https://www.youtube.com/results?search_query='+search, headers=headers).text
-  soup = BeautifulSoup(html, 'html.parser')
-  for link in soup.find_all('a'):
-    if '/watch?v=' in link.get('href'):
-        # May change when Youtube Website may get updated in the future.
-        video_link = link.get('href') 
-        break
-  video_link =  'http://www.youtube.com/'+video_link
-  command = ('youtube-dl --extract-audio --audio-format mp3 --audio-quality ' + QUALITY + ' ' + video_link)	
-  os.system(command)    
+    os.system("instantmusic -q -s "+name)
+    
+
+
+@register(outgoing=True, pattern="^.song(?: |$)(.*)")
+async def _(event):
+    if event.fwd_from:
+        return
+    link = event.pattern_match.group(1)
+    chat = "@SpotifyMusicDownloaderBot"
+    await event.edit("```Getting Your Music```")
+    async with bot.conversation(chat) as conv:
+          await asyncio.sleep(2)
+          await event.edit("`Downloading music taking some times,  Stay Tuned.....`")
+          try:
+              response = conv.wait_event(events.NewMessage(incoming=True,from_users=752979930))
+              await bot.send_message(chat, link)
+              respond = await response
+          except YouBlockedUserError:
+              await event.reply("```Please unblock @SpotifyMusicDownloaderBot and try again```")
+              return
+          await event.delete()
+          await bot.forward_messages(event.chat_id, respond.message)
+
+CMD_HELP.update({
+        "music":
+        ".song`<Artist - Song Title>\
+            \n\n\nUsage:For searching songs from Spotify."
+})
